423b7c5b033694be88e15bc8898dfe7b295227e9
yihang.gong <yihang.gong@garena.com>
2025-09-17 11:12:20 +0800

    Fix namespace issue

--- ClosedXML/Utils/ConcurrentList.cs
+++ ClosedXML/Utils/ConcurrentList.cs
@@ -6,92 +6,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BrushPot
+namespace ClosedXML.Utils;
+
+public class ConcurrentList<T> : IEnumerable<T>, ICollection<T> where T : notnull
 {
-    public class ConcurrentList<T> : IEnumerable<T>, ICollection<T> where T : notnull
+    ConcurrentDictionary<T, object?> innerDictionary = new();
+
+    public int Count => innerDictionary.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T item)
     {
-        ConcurrentDictionary<T, object?> innerDictionary = new();
+        innerDictionary.TryAdd(item, null);
+    }
 
-        public int Count => innerDictionary.Count;
+    public void Clear()
+    {
+        innerDictionary.Clear();
+    }
 
-        public bool IsReadOnly => false;
+    public bool Contains(T item)
+    {
+        return innerDictionary.ContainsKey(item);
+    }
 
-        public void Add(T item)
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        innerDictionary.Keys.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return innerDictionary.Keys.GetEnumerator();
+    }
+
+    public bool Remove(T item)
+    {
+        return innerDictionary.TryRemove(item, out _);
+    }
+
+    public void RemoveAll(Predicate<T> predicate)
+    {
+        foreach (var item in innerDictionary.Keys)
         {
-            innerDictionary.TryAdd(item, null);
+            if (predicate(item))
+                Remove(item);
+        }
+    }
+
+    public List<T> FindAll(Predicate<T> predicate)
+    {
+        var span = new List<T>();
+        foreach (var item in innerDictionary.Keys)
+        {
+            if (predicate(item))
+                span.Add(item);
         }
 
-        public void Clear()
+        return span;
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public T this[int i]
+    {
+        get
         {
-            innerDictionary.Clear();
+            if (i < 0 || i >= innerDictionary.Count)
+                throw new ArgumentOutOfRangeException(nameof(i), "Index is out of range.");
+            return innerDictionary.Keys.ElementAt(i);
         }
-
-        public bool Contains(T item)
+        set
         {
-            return innerDictionary.ContainsKey(item);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            innerDictionary.Keys.CopyTo(array, arrayIndex);
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return innerDictionary.Keys.GetEnumerator();
-        }
-
-        public bool Remove(T item)
-        {
-            return innerDictionary.TryRemove(item, out _);
-        }
-
-        public void RemoveAll(Predicate<T> predicate)
-        {
-            foreach (var item in innerDictionary.Keys)
-            {
-                if (predicate(item))
-                    Remove(item);
-            }
-        }
-
-        public List<T> FindAll(Predicate<T> predicate)
-        {
-            var span = new List<T>();
-            foreach (var item in innerDictionary.Keys)
-            {
-                if (predicate(item))
-                    span.Add(item);
-            }
-
-            return span;
-        }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public T this[int i]
-        {
-            get
-            {
-                if (i < 0 || i >= innerDictionary.Count)
-                    throw new ArgumentOutOfRangeException(nameof(i), "Index is out of range.");
-                return innerDictionary.Keys.ElementAt(i);
-            }
-            set
-            {
-                if (i < 0 || i >= innerDictionary.Count)
-                    throw new ArgumentOutOfRangeException(nameof(i), "Index is out of range.");
-                Remove(innerDictionary.Keys.ElementAt(i));
-                Add(value);
-            }
+            if (i < 0 || i >= innerDictionary.Count)
+                throw new ArgumentOutOfRangeException(nameof(i), "Index is out of range.");
+            Remove(innerDictionary.Keys.ElementAt(i));
+            Add(value);
         }
     }
 }
